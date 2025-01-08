48583f25e7fd9fb960bf9a34ae7ea5a9f4cc9a08
jisoohan <hanjisoo93@gmail.com>
2025-01-08 23:13:02 +0900

    feat: build.gradle.kts에 새로운 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.4.1"
 	id("io.spring.dependency-management") version "1.1.7"
-	id("org.openapi.generator") version "6.6.0"
 }
 
 fun getGitHash(): String {
@@ -35,27 +34,28 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+
+	// Swagger
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
 
     // DB
 	runtimeOnly("com.mysql:mysql-connector-j")
 
-    // Test
+    // Test & Docker testContainer
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.boot:spring-boot-testcontainers")
 	testImplementation("org.testcontainers:junit-jupiter")
 	testImplementation("org.testcontainers:mysql")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
-	// Swagger
-	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
-	implementation("org.jetbrains:annotations:24.0.0")
-	implementation("javax.annotation:javax.annotation-api:1.3.2")
-	implementation("org.openapitools:openapi-generator:6.6.0")
-
 	// Lombok
 	implementation("org.projectlombok:lombok:1.18.26")
 	annotationProcessor("org.projectlombok:lombok:1.18.26")
 	testAnnotationProcessor("org.projectlombok:lombok:1.18.26")
+
+	// Bean Validation API
+	implementation("jakarta.validation:jakarta.validation-api:3.0.2")
 }
 
 tasks.withType<Test> {
