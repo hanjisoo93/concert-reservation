dfce40d5a97d8ff93f6eb8b5f08823d77bdd8703
jisoohan <hanjisoo93@gmail.com>
2025-01-09 00:20:56 +0900

    feat(reservation): Reservation infra 구현

---
+++ src/main/java/kr/hhplus/be/server/domain/reservation/entity/ReservationStatus.java
@@ -0,0 +1,15 @@
+package kr.hhplus.be.server.domain.reservation.entity;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ReservationStatus {
+
+    PENDING("예약 중"),
+    SUCCESS("성공"),
+    FAIL("실패");
+
+    private final String text;
+}
