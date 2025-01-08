126a9088dc18939c4549072b8b5f106d1ed68672
jisoohan <hanjisoo93@gmail.com>
2025-01-08 23:55:28 +0900

    feat(user): 사용자 infra 구현

---
+++ src/main/java/kr/hhplus/be/server/domain/user/entity/User.java
@@ -0,0 +1,22 @@
+package kr.hhplus.be.server.domain.user.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Builder
+    private User(Long id) {
+        this.id = id;
+    }
+}
