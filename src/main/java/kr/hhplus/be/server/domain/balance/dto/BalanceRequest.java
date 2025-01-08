33c8f2f7e14993fe42b4e220db758acddaf89e5e
jisoohan <hanjisoo93@gmail.com>
2025-01-08 23:20:07 +0900

    chore: 레포지토리 구조 변경에 따른 불필요한 파일 삭제

--- src/main/java/kr/hhplus/be/server/domain/balance/dto/BalanceRequest.java
+++
@@ -1,9 +0,0 @@
-package kr.hhplus.be.server.domain.balance.dto;
-
-import lombok.Data;
-
-@Data
-public class BalanceRequest {
-    private String uuid;
-    private Double amount;
-}
--- src/main/java/kr/hhplus/be/server/domain/concert/seat/dto/SeatReservationRequest.java
+++
@@ -1,8 +0,0 @@
-package kr.hhplus.be.server.domain.concert.seat.dto;
-
-import lombok.Data;
-
-@Data
-public class SeatReservationRequest {
-    private String ownerUuid;
-}
