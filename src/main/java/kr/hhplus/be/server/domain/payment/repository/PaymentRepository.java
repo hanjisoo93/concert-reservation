fab5e9663ec3cc714a15eaa036e0d2bf73014567
Jisoo Han <hanjisoo93@gmail.com>
2025-01-12 10:34:12 +0900

    Revert "[STEP-07] 콘서트 예약 서비스 단위테스트"

--- src/main/java/kr/hhplus/be/server/domain/payment/repository/PaymentRepository.java
+++
@@ -1,9 +0,0 @@
-package kr.hhplus.be.server.domain.payment.repository;
-
-import kr.hhplus.be.server.domain.payment.entity.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
-}
