fc34bf2297c7ad6629b307bfd7ddd6bdf0c0cfd1
jisoohan <hanjisoo93@gmail.com>
2025-01-15 01:15:46 +0900

    feat: 공통 예외 처리 로직 추가

---
+++ src/main/java/kr/hhplus/be/server/common/exception/BaseException.java
@@ -0,0 +1,14 @@
+package kr.hhplus.be.server.common.exception;
+
+public class BaseException extends RuntimeException{
+    private final String errorCode;
+
+    public BaseException(String message, String errorCode) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+
+    public String getErrorCode() {
+        return errorCode;
+    }
+}
