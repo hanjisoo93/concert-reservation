bdff0ca67984ba54350747e9f3bc7737d15d8207
jisoohan <hanjisoo93@gmail.com>
2025-01-15 01:16:17 +0900

    feat: ReservationException 추가

---
+++ src/main/java/kr/hhplus/be/server/domain/exception/reservation/ReservationException.java
@@ -0,0 +1,7 @@
+package kr.hhplus.be.server.domain.exception.reservation;
+
+public class ReservationException extends RuntimeException{
+    public ReservationException(String message){
+        super(message);
+    }
+}
