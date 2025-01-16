05f761caca20be005e68d4329d0ef251fcd715d2
jisoohan <hanjisoo93@gmail.com>
2025-01-17 05:11:00 +0900

    refactor(point): 포인트 동시성 통합 테스트

--- src/main/java/kr/hhplus/be/server/infra/repository/point/PointRepository.java
+++ src/main/java/kr/hhplus/be/server/infra/repository/point/PointRepository.java
@@ -1,7 +1,11 @@
 package kr.hhplus.be.server.infra.repository.point;
 
+import jakarta.persistence.LockModeType;
 import kr.hhplus.be.server.domain.entity.point.Point;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,4 +13,8 @@ import java.util.Optional;
 @Repository
 public interface PointRepository extends JpaRepository<Point, Long> {
     Optional<Point> findPointByUserId(Long userId);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)  // 비관적 락 적용
+    @Query("SELECT p FROM Point p WHERE p.userId = :userId")
+    Optional<Point> findPointForUpdateByUserId(@Param("userId") Long userId);
 }
