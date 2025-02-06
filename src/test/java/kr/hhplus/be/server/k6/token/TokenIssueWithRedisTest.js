226ca6db22b1946a7feee5ce6d47262154902125
jisoohan <hanjisoo93@gmail.com>
2025-02-07 00:04:48 +0900

    test(token): K6 성능 테스트 코드 추가 (조회/생성)

---
+++ src/test/java/kr/hhplus/be/server/k6/token/TokenIssueWithRedisTest.js
@@ -0,0 +1,22 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export let options = {
+    stages: [
+        { duration: '10s', target: 1300 }, // 10초 동안 1300개의 동시 요청 유지
+    ],
+};
+
+export default function () {
+    let userId = Math.floor(Math.random() * 100000);
+    let url = `http://localhost:8080/api/tokens/issue/redis?userId=${userId}`;  // Redis 기반 API
+    let res = http.post(url, JSON.stringify({ userId }), { headers: { 'Content-Type': 'application/json' } });
+
+    console.log(`Response status: ${res.status}, Body: ${res.body}`);
+
+    check(res, {
+        'is status 200': (r) => r.status === 200,
+    });
+
+    sleep(1);
+}
---
+++ src/test/java/kr/hhplus/be/server/k6/token/TokenWithRedisTest.js
@@ -0,0 +1,36 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export let options = {
+    stages: [
+        { duration: '10s', target: 1300 }, // 10초 동안 1300개의 동시 요청 유지
+    ],
+};
+
+export function setup() {
+    let successfulUserIds = [];
+    for (let id = 1; id <= 1300; id++) {
+        let url = `http://localhost:8080/api/tokens/issue/redis?userId=${id}`;
+        let payload = JSON.stringify({ userId: id });
+        let params = { headers: { 'Content-Type': 'application/json' } };
+        let res = http.post(url, payload, params);
+
+        if (res.status === 200) {
+            successfulUserIds.push(id);
+        }
+    }
+    return successfulUserIds;
+}
+
+
+export default function(data) {
+    let userId = data[Math.floor(Math.random() * data.length)];
+    let url = `http://localhost:8080/api/tokens/get/redis?userId=${userId}`;
+    let res = http.get(url);
+
+    check(res, {
+        'status is 200': (r) => r.status === 200,
+    });
+
+    sleep(1);
+}
